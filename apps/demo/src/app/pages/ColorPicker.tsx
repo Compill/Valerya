c730b4c28fb7c385f099f8df2cdc4581932215ad
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-08-11 16:26:14 +0100

    [CORE] Build alpha palette function and surface/palette demo [PROJECT] Downgraded typescript from 4.7 to 4.6 because VSCode autocompletion was not working anymore

--- apps/demo/src/app/pages/ColorPicker.tsx
+++ apps/demo/src/app/pages/ColorPicker.tsx
@@ -1,6 +1,6 @@
 import { Container, Surface, SurfaceProps, SurfaceSchemeVariant } from "@katia/components";
 import { buildAlphaSurface, buildSurfaceFromColor, SurfaceSchemeSet } from "@katia/core";
-import { SoperioComponent, useDarkMode } from "@soperio/react";
+import { darken, lighten, SoperioComponent, useDarkMode } from "@soperio/react";
 import React from "react";
 import { SketchPicker } from "react-color"
 
@@ -13,37 +13,42 @@ import { SketchPicker } from "react-color"
 
 // TODO Add a scale from -1 to +1 to alter alpha values of colors in scheme set
 
+// TODO Build the palette
+
 
 export default function Page({ ...props })
 {
-  const [color, setColor] = React.useState({ hex: "#ff475569" })
-  const [coef, setCoef] = React.useState(0.00)
+  const [color, setColor] = React.useState({ hex: "#0099ffff" })
+  const [coef, setCoef] = React.useState("0.00")
 
-  const scheme = buildAlphaSurface(parseInt("ff" + color.hex.substring(1), 16), { coef })
+  const scheme = buildAlphaSurface(parseInt("ff" + color.hex.substring(1), 16), { coef: parseFloat(coef) })
 
   const handleChange = React.useCallback(color => setColor(color), [setColor])
   const handleCoefChange = React.useCallback(event => setCoef(event.target.value), [setCoef])
 
   return (
-    <div w="full" h="screen">
+    <div w="full" h="full" >
       <Header color={color.hex} onChange={handleChange} />
+
+      <Palette color={color.hex} coef={coef} />
 
       <div dflex flexRow w="full" alignItems="center" placeContent="center">
         <span>Adjust colors</span>
-        
+
         <input
           mx="3"
           w="20"
           type="range"
           min={-0.5}
           max={0.5}
-          step={0.01}
+          step={0.001}
           value={coef}
           onChange={handleCoefChange} />
 
-          <span>{coef}</span>
+        <span>{addZeroes(coef, 3)}</span>
 
       </div>
+
 
       <Container center size="x2" dflex flexCol gap="20" alignItems="center" justifyContent="center" py="20" fontWeight="600" fontSize="x4">
 
@@ -87,9 +92,9 @@ function SurfaceBlock({ scheme, schemeVariant }: SurfaceBlockProps)
   return (
     <div dflex flexCol gap="10">
 
-      <div trait="typo.h5" dflex flexCol placeContent="center" alignItems="center" w="16">{schemeVariant}</div>
+      <div trait="typo.h5" fontSize="sm" textAlign="center" w="full" dflex flexCol placeContent="center" alignItems="center">{schemeVariant}</div>
 
-      <Surface scheme={scheme} schemeVariant={schemeVariant} {...sfProps}>
+      <Surface scheme={scheme} schemeVariant={schemeVariant} {...sfProps} w="40">
         <div trait="typo.h3">Title</div>
         <div trait="typo.subtitle1">Subtitle</div>
       </Surface>
@@ -108,8 +113,41 @@ function Header({ color, onChange }: HeaderProps)
 
 
   return (
-    <div dflex w="full" p="10" alignItems="center" placeContent="center">
+    <div dflex w="full" pt="5" alignItems="center" placeContent="center">
       <SketchPicker color={color} onChange={onChange} />
     </div>
   )
+}
+
+interface PaletteProps
+{
+  color: string,
+  coef: string
+}
+
+function Palette({ color, coef }: PaletteProps)
+{
+  console.log("coef", coef, typeof coef)
+  const transformCoef = (parseFloat(coef) + 0.5) * 5
+  const leveledCoef = 4 + transformCoef
+  const colors = [color]
+
+  for (let i = 0; i < 5; i++)
+    colors.push(lighten(color, leveledCoef + leveledCoef * i))
+
+  for (let i = 0; i < 5; i++)
+    colors.unshift(darken(color, leveledCoef + leveledCoef * i))
+
+  return (
+    <div w="full" p="10" alignItems="center" placeContent="center" dflex flexRow>
+      {colors.map((color, index) => <div key={index} w="10" h="10" bgColor={color} />)}
+    </div>
+  )
+}
+
+function addZeroes(num, zeroes)
+{
+  const dec = String(num).split('.')[1]
+  const len = dec && dec.length > zeroes ? dec.length : zeroes
+  return Number(num).toFixed(len)
 }
---
+++ libs/core/src/lib/surface/buildAlphaPalette.ts
@@ -0,0 +1,4 @@
+export function buildAlphaPalette(color: number)
+{
+    
+}
