6fc58764084d9a9ca700ea94cd70685ae916c278
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2023-05-28 20:57:30 +0700

    [SURFACE] Version 0.0.43

--- libs/core/src/lib/config/useMergeComponentConfig.ts
+++ libs/core/src/lib/config/useMergeComponentConfig.ts
@@ -2,12 +2,12 @@ import { useThemeExtra } from "@soperio/react"
 import { ComponentManager } from "./ComponentManager"
 import deepmerge from "deepmerge";
 
-export function useMergedComponentConfig<T>(component: string)
+export function useMergedComponentConfig(component: string)
 {
     const themeComponents = useThemeExtra("valerya.components")
 
     const themeConfig = themeComponents?.[component]
-    const defaultConfig = ComponentManager.getComponentConfig(component) as T
+    const defaultConfig = ComponentManager.getComponentConfig(component) ?? {}
 
     return themeConfig ? deepmerge(defaultConfig, themeConfig as any) : defaultConfig
 }
