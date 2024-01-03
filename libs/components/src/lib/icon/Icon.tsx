92c9430f72e9db1227f683bcd76aa53b5b8e13fb
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2024-01-03 16:50:44 +0000

    [COMPONENTS] Icon forwardRef

--- libs/components/src/lib/icon/Icon.tsx
+++ libs/components/src/lib/icon/Icon.tsx
@@ -1,4 +1,4 @@
-import { ParentComponent } from "@soperio/react";
+import { ParentComponent, forwardRef } from "@soperio/react";
 import defaultConfig from "./config";
 import { ComponentProps, ExtendConfig } from "./types";
 import { ComponentManager, useComponentConfig } from "@valerya/core";
@@ -17,16 +17,16 @@ export interface IconProps extends ComponentProps, ParentComponent
 /**
  * A component to display an svg icon
  */
-export function Icon({ path, title, size, config, children, ...props }: IconProps)
+export const Icon = forwardRef<"span", IconProps>(({ path, title, size, config, children, ...props }: IconProps, ref) =>
 {
   const styles = useComponentConfig(COMPONENT_ID, config, { size }, props)
 
   return (
-    <span display="block" {...styles} {...props}>
+    <span ref={ref} display="block" {...styles} {...props}>
       <svg viewBox="0 0 24 24">
         {title != null && <title>{title}</title>}
         <path d={path} fill="currentColor" />
       </svg>
     </span>
   );
-}
+})
