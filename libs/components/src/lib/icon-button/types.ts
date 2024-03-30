75e75a24ddadb51566e76365294cc2b54df66ebc
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2024-03-30 13:12:31 +0700

    [COMPONENTS] Update IconButton types to include as

--- libs/components/src/lib/icon-button/types.ts
+++ libs/components/src/lib/icon-button/types.ts
@@ -1,10 +1,10 @@
-import { SoperioComponent } from "@soperio/react";
+import { HTMLButtonProps, HTMLDivProps, RightJoinProps, SoperioComponent } from "@soperio/react";
 import { ComponentConfig, ComponentTypings, ExtendComponentConfig } from "@valerya/core";
 
 
 type TraitProps = ComponentTypings<"Valerya.IconButton">;
 
-export type ComponentProps = SoperioComponent & TraitProps
+export type ComponentProps = SoperioComponent & RightJoinProps<HTMLButtonProps, HTMLDivProps>
 
 export type Config = ComponentConfig<TraitProps>;
 export type ExtendConfig = ExtendComponentConfig<Config>;
