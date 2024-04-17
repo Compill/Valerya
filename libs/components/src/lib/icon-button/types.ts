0ca0616e25c2901a3ee2cb0bb33af72331e730f7
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2024-04-17 15:16:58 +0700

    [COMPONENTS] Button: fix type

--- libs/components/src/lib/icon-button/types.ts
+++ libs/components/src/lib/icon-button/types.ts
@@ -1,10 +1,12 @@
-import { HTMLButtonProps, HTMLDivProps, RightJoinProps, SoperioComponent } from "@soperio/react";
-import { ComponentConfig, ComponentTypings, ExtendComponentConfig } from "@valerya/core";
+import { ComponentConfig, ComponentTypings, DisabledState, ExtendComponentConfig, SelectedState } from "@valerya/core";
+import { ButtonProps } from "../button";
+import { HoverableSurfaceBasedComponent } from "../surface";
+import { HTMLButtonProps, HTMLDivProps, RightJoinProps } from "@soperio/react";
 
 
 type TraitProps = ComponentTypings<"Valerya.IconButton">;
 
-export type ComponentProps = SoperioComponent & RightJoinProps<HTMLButtonProps, HTMLDivProps>
+export type ComponentProps = HoverableSurfaceBasedComponent<TraitProps & SelectedState & DisabledState & RightJoinProps<HTMLButtonProps, HTMLDivProps>>
 
 export type Config = ComponentConfig<TraitProps>;
 export type ExtendConfig = ExtendComponentConfig<Config>;
