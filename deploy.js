9ed656e3daf90723f4fff8b7594377ccb24129e3
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2025-11-04 15:43:38 +0700

    [UI] Version 0.0.123

--- deploy.js
+++ deploy.js
@@ -24,7 +24,7 @@ function deploy(lib)
   console.log(`Updated package.json to version ${version}`);
 
   console.log("Deploying...");
-  execSync(`nx run ${lib}:deploy --package-version ${version}`, { stdio: 'inherit' });
+  execSync(`nx run ${lib}:deploy --package-version ${version} --verbose`, { stdio: 'inherit' });
   console.log("Deployed!");
 
   console.log("Committing...");
