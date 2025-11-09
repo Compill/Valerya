0d2bb775bf576a4ace98b64a9e416a59ece71db5
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2025-11-09 14:17:18 +0700

    Updating Cli bin to handle ESM

--- libs/cli/bin/index.js
+++ libs/cli/bin/index.js
@@ -1,8 +1,16 @@
-#!/usr/bin/env node --experimental-specifier-resolution=node
-const { run } = require("../dist/index.js");
+#!/usr/bin/env node
+import { spawn } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 
-run().catch((e) =>
-{
-    console.error(e);
-    process.exit(1);
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const entry = resolve(__dirname, "../dist/index.js");
+
+// Run Node with the flag so directory imports work properly
+const child = spawn(
+  process.execPath,
+  [ "--experimental-specifier-resolution=node", entry, ...process.argv.slice(2) ],
+  { stdio: "inherit" }
+);
+
+child.on("exit", (code) => process.exit(code));
